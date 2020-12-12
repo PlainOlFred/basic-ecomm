3d19c8be2db61956fdc50a8dcc21542882df9674
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-12-12 14:02:47 -0600

    Update header and nav items
    items include logo, search input and nav items to manage
    cart, orders, account, and sign in/out
    *only rendering items no functionality

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,25 +4,30 @@ import styles from "./Header.module.css";
 function Header() {
     return ( 
         <div className={styles.header}>
-            
+
           <h1 className={styles.header__logo}>Ecomm</h1>
 
           <div className={styles.header__search}>
               <input className={styles.header__searchInput}/>
+              <button className={styles.header__searchIcon}>Search</button>
           </div>
 
           <div className={styles.header__nav}>
               <div className={styles.header__navItem}>
-                  <span className={styles.header__navOption_top}>Welcome</span>
-                  <span className={styles.header__navOption_bottoom}>Sign In</span>
+                  <span className={styles.header__navOption_top}>Cart <span className={styles.header__cart}>0</span></span>
+                  <span className={styles.header__navOption_bottoom}><span >Checkout</span></span>
               </div>
               <div className={styles.header__navItem}>
-                  <span className={styles.header__navOption_top}>Track</span>
-                  <span className={styles.header__navOption_bottoom}>Order</span>
+                  <span className={styles.header__navOption_top}>Track <span className={styles.header__orders}>0</span></span>
+                  <span className={styles.header__navOption_bottoom}>Orders</span>
               </div>
               <div className={styles.header__navItem}>
                   <span className={styles.header__navOption_top}>Manange</span>
                   <span className={styles.header__navOption_bottoom}>Your Account</span>
+              </div>
+              <div className={styles.header__navItem}>
+                  <span className={styles.header__navOption_top}>Welcome</span>
+                  <span className={styles.header__navOption_bottoom}>Sign In</span>
               </div>
           </div>
         </div>
