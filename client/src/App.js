cfd65e4e4e80d2c92a8b8408bbc881b6bfc0a6ca
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-12-12 18:21:56 -0600

    Intial Landing page without products

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+import HomePage from './containers/HomePage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Header */}
       <Header />
       {/* Home */}
+      <HomePage />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from "./Home.module.css";
+
+function Home(props) {
+    return ( 
+        <div className={styles.home}>
+            <div className={styles.home__container}>
+
+            <h1>homepage</h1>
+
+            {/*  */}
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
