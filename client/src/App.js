6c5d9608272b59071e413e06341f7366aaffc6c6
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-12-12 21:12:37 -0600

    Implemented React-router-dom
    Add h1 tag as placeholder for /cart route

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import './App.css';
-import Header from './components/Header/Header';
-import HomePage from './containers/HomePage';
+import Header from "./components/Header/Header";
+import HomePage from "./containers/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <HomePage />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route path='/cart'>
+            <h1>CArt</h1>
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
